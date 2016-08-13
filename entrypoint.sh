171f567c6aa673e2511e03bf0ae20a6a86cd47ee
Chanchai Junlouchai <neverlock@gmail.com>
2016-08-13 03:58:20 +0000

    nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+echo "127.0.0.1		$TARGET" >> /etc/hosts
 nginx -g "daemon off;" &
 telegraf &
 sleep 5
-/usr/local/bin/wrk -c${CONNECTION} -d${DELAY}s -t${THREAD} $TARGET
+/usr/local/bin/wrk -c${CONNECTION} -d${DELAY}s -t${THREAD} http://$TARGET
